77c8991e0b55d37793823411b2e66363283f941f
Lacky Kannauje <lkgamer146@gmail.com>
2024-05-02 15:50:52 +0530

    frontend changes

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 require("dotenv").config();
 const discord_apikey = process.env.DISCORD_API_KEY;
-const express = require('express');
-const {handleDiscord} = require('./controller/discord')
+const express = require("express");
+const { handleDiscord } = require("./controller/discord");
 const { Client, GatewayIntentBits } = require("discord.js");
+const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-app.get('/',(req, res)=>{
-  res.send('Connected!!');
-})
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 const client = new Client({
   intents: [
@@ -23,4 +26,4 @@ client.on("messageCreate", handleDiscord);
 
 client.login(discord_apikey);
 
-app.listen(PORT,()=>console.log(`server running at ${PORT}`));
+app.listen(PORT, () => console.log(`server running at ${PORT}`));
